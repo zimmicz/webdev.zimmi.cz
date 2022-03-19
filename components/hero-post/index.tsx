a055c930a465409885436db3becfba955475478e
Michal Zimmermann <zimmicz@gmail.com>
2022-03-19 16:57:08 +0100

    refactor: drop bg colors

--- components/hero-post/index.tsx
+++ components/hero-post/index.tsx
@@ -5,49 +5,49 @@ import { ReadTimeResults } from 'reading-time';
 import { BannerImage } from '../banner-image';
 
 type Props = {
-    code: string;
-    frontmatter: {
-        image?: {
-            url: string;
-            aspectRatio: string;
-            credit: string;
-            width: number;
-            height: number;
-        };
-        publishedAt: string;
-        tags: Array<string>;
-        title: string;
+  code: string;
+  frontmatter: {
+    image?: {
+      url: string;
+      aspectRatio: string;
+      credit: string;
+      width: number;
+      height: number;
     };
-    readingTime: ReadTimeResults;
-    slug: string;
+    publishedAt: string;
+    tags: Array<string>;
+    title: string;
+  };
+  readingTime: ReadTimeResults;
+  slug: string;
 };
 
 const HeroPost = ({ frontmatter, ...props }: Props) => {
-    const Component = React.useMemo(() => getMDXComponent(props.code), [props.code]);
+  const Component = React.useMemo(() => getMDXComponent(props.code), [props.code]);
 
-    return (
-        <article className="group p-20 w-full bg-gray-100 text-st-patricks-blue leading-10 gap-8 flex flex-wrap">
-            <div className="max-w-screen-xl mx-auto">
-                <div className="flex flex-wrap xl:flex-nowrap lg:justify-left lg:items-center gap-8">
-                    <h1 className="text-3xl w-2/3 flex-shrink-0 lg:text-7xl font-title font-bold text-lavender">
-                        <Link href="/">
-                            <a className="text-decoration-fade from-lavender to-lavender py-1">{frontmatter.title}</a>
-                        </Link>
-                    </h1>
-                    {frontmatter.image ? (
-                        <BannerImage
-                            src={frontmatter.image.url}
-                            credit={frontmatter.image.credit}
-                            aspectRatio={frontmatter.image.aspectRatio}
-                            width={frontmatter.image.width}
-                            height={frontmatter.image.height}
-                        />
-                    ) : null}
-                </div>
-                <Component />
-            </div>
-        </article>
-    );
+  return (
+    <article className="group p-20 w-full bg-gray-100 text-st-patricks-blue leading-10 gap-8 flex flex-wrap">
+      <div className="max-w-screen-xl mx-auto">
+        <div className="flex flex-wrap xl:flex-nowrap lg:justify-left lg:items-center gap-8">
+          <h1 className="text-3xl w-2/3 flex-shrink-0 lg:text-7xl font-title font-bold text-lavender">
+            <Link href="/">
+              <a className="text-decoration-fade from-lavender to-lavender py-1">{frontmatter.title}</a>
+            </Link>
+          </h1>
+          {frontmatter.image ? (
+            <BannerImage
+              src={frontmatter.image.url}
+              credit={frontmatter.image.credit}
+              aspectRatio={frontmatter.image.aspectRatio}
+              width={frontmatter.image.width}
+              height={frontmatter.image.height}
+            />
+          ) : null}
+        </div>
+        <Component />
+      </div>
+    </article>
+  );
 };
 
 export { HeroPost };
--- components/post/index.tsx
+++ components/post/index.tsx
@@ -6,56 +6,56 @@ import { PostCtaLink, Typography, WrittenAt } from '..';
 import { BannerImage } from '../banner-image';
 
 const Post = ({ code, frontmatter, readingTime, slug }: Post) => {
-    const Component = React.useMemo(() => getMDXComponent(code), [code]);
-    const { publishedAt, title, tags } = frontmatter;
+  const Component = React.useMemo(() => getMDXComponent(code), [code]);
+  const { publishedAt, title, tags } = frontmatter;
 
-    return (
-        <motion.section
-            initial="rest"
-            whileHover="hover"
-            className="group transition-colors rounded-lg bg-st-patricks-blue text-gray-300 p-4 sm:p-10 leading-10 sm:leading-10 sm:text-base border-transparent flex flex-col gap-6 lg:gap-8 hover:shadow-sm"
-        >
-            <Typography.H1>
-                <Link href={`/posts/${slug}`}>
-                    <a className="text-decoration-fade from-lavender to-lavender pb-2">{title}</a>
-                </Link>
-            </Typography.H1>
-            <div className="flex flex-wrap justify-between text-base gap-2">
-                <WrittenAt date={publishedAt} />
-                <ul className="flex space-x-4">
-                    {tags.map((tag) => (
-                        <li key={tag}>
-                            <Link href="/tags/react">
-                                <a className="border-lavender border py-1 px-4 rounded-lg from-lavender to-lavender tag-decoration-fade">
-                                    <small>{tag}</small>
-                                </a>
-                            </Link>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-            {frontmatter.image ? (
-                <BannerImage
-                    credit={frontmatter.image.credit}
-                    url={frontmatter.image.url}
-                    aspectRatio={frontmatter.image.aspectRatio}
-                    width={frontmatter.image.width}
-                    height={frontmatter.image.height}
-                />
-            ) : null}
-            <Component
-                components={{
-                    h2: Typography.H2,
-                    h3: Typography.H3,
-                    h4: Typography.H4,
-                    h5: Typography.H5,
-                    h6: Typography.H6,
-                }}
-            />
+  return (
+    <motion.section
+      initial="rest"
+      whileHover="hover"
+      className="group transition-colors rounded-lg text-gray-300 p-4 sm:p-10 leading-10 sm:leading-10 sm:text-base border-transparent flex flex-col gap-6 lg:gap-8 hover:shadow-sm"
+    >
+      <Typography.H1>
+        <Link href={`/posts/${slug}`}>
+          <a className="text-decoration-fade from-lavender to-lavender pb-2">{title}</a>
+        </Link>
+      </Typography.H1>
+      <div className="flex flex-wrap justify-between text-base gap-2">
+        <WrittenAt date={publishedAt} />
+        <ul className="flex space-x-4">
+          {tags.map((tag) => (
+            <li key={tag}>
+              <Link href="/tags/react">
+                <a className="border-lavender border py-1 px-4 rounded-lg from-lavender to-lavender tag-decoration-fade">
+                  <small>{tag}</small>
+                </a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+      {frontmatter.image ? (
+        <BannerImage
+          credit={frontmatter.image.credit}
+          url={frontmatter.image.url}
+          aspectRatio={frontmatter.image.aspectRatio}
+          width={frontmatter.image.width}
+          height={frontmatter.image.height}
+        />
+      ) : null}
+      <Component
+        components={{
+          h2: Typography.H2,
+          h3: Typography.H3,
+          h4: Typography.H4,
+          h5: Typography.H5,
+          h6: Typography.H6,
+        }}
+      />
 
-            <PostCtaLink slug={slug} readingTime={readingTime} />
-        </motion.section>
-    );
+      <PostCtaLink slug={slug} readingTime={readingTime} />
+    </motion.section>
+  );
 };
 
 export { Post };
