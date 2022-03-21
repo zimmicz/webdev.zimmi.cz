5ec50c7dcb8dc45e77e71b09b9fb2b13e8b9ec9d
Michal Zimmermann <zimmicz@gmail.com>
2022-03-21 15:28:48 +0100

    fix: menu link ref

--- components/menu-link/index.tsx
+++ components/menu-link/index.tsx
@@ -4,18 +4,26 @@ import { motion } from 'framer-motion';
 
 type LinkProps = Pick<React.ComponentProps<typeof Link>, 'href' | 'children'> & { onClick: React.MouseEventHandler };
 
-const MenuLink = ({ onClick, href, children, className }: { className?: string } & Partial<LinkProps>) => {
-  return (
-    <motion.a
-      animate={{ x: 0 }}
-      initial={{ x: 50 }}
-      onClick={onClick}
-      href={href?.toString()}
-      className={`${className} pb-2 text-primary text-decoration-fade from-primary to-primary`}
-    >
-      {children}
-    </motion.a>
-  );
-};
+const MenuLink = React.forwardRef(
+  (
+    { onClick, href, children, className }: { className?: string } & Partial<LinkProps>,
+    ref: React.Ref<HTMLAnchorElement>,
+  ) => {
+    return (
+      <motion.a
+        ref={ref}
+        animate={{ x: 0 }}
+        initial={{ x: 50 }}
+        onClick={onClick}
+        href={href?.toString()}
+        className={`${className} pb-2 text-primary text-decoration-fade from-primary to-primary`}
+      >
+        {children}
+      </motion.a>
+    );
+  },
+);
+
+MenuLink.displayName = 'MenuLink';
 
 export { MenuLink };
