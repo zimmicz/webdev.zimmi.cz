6d12cbdc7e96d5bfcbd6717c872c833dfe5a44d0
Michal Zimmermann <zimmicz@gmail.com>
2022-03-25 20:19:59 +0100

    fix: ignore sidenote type error

--- components/sidenote/index.tsx
+++ components/sidenote/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import '../../public/js/sidenote';
 
+// @ts-expect-error my-sidenote does not exist in JSX namespace
 const Sidenote = ({ children }: React.PropsWithChildren<unknown>) => <my-sidenote>{children}</my-sidenote>;
 
 export default Sidenote;
