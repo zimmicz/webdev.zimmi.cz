94fe43d39d9e9b3dd98c639b698bfa05f38bf624
Michal Zimmermann <zimmicz@gmail.com>
2022-04-07 08:23:38 +0200

    fix: category listing

--- pages/categories/[category].tsx
+++ pages/categories/[category].tsx
@@ -4,19 +4,19 @@ import { getPublished, getAllCategories } from '../../lib/utils';
 import Tag from '../../public/icons/tag.svg';
 
 type Props = {
-  posts: PromiseReturnType<ReturnType<typeof getPublished>>;
+  items: PromiseReturnType<ReturnType<typeof getPublished>>;
   category: Post['frontmatter']['categories'][number];
 };
 
-const Category = ({ category, posts }: Props) => {
+const Category = ({ category, items }: Props) => {
   return (
     <Layout>
       <Typography.H2 className="py-4 capitalize flex items-center space-x-2">
         <span>{category}</span>
         <Tag className="mt-[7px] lg:mt-[14px]" />
       </Typography.H2>
-      {posts.map((post) => (
-        <Teaser key={post.slug} {...post} />
+      {items.map((item) => (
+        <Teaser key={item.slug} {...item} />
       ))}
     </Layout>
   );
@@ -24,11 +24,14 @@ const Category = ({ category, posts }: Props) => {
 
 const getStaticProps = async ({ params }: { params: { category: Props['category'] } }) => {
   const posts = await getPublished('post');
-  const filteredPosts = posts.filter((post) => post.frontmatter.categories.includes(params.category));
+  const snippets = await getPublished('snippet');
+  const items = [...posts, ...snippets];
+
+  const filtered = items.filter((item) => item.frontmatter.categories.includes(params.category));
 
   return {
     props: {
-      posts: filteredPosts,
+      items: filtered,
       category: params.category,
     },
   };
