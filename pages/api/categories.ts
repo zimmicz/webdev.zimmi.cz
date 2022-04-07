965d300eb0af531d7e145d437a55dce2a52b67e9
Michal Zimmermann <zimmicz@gmail.com>
2022-04-07 08:39:34 +0200

    style: underscore variable

--- pages/api/categories.ts
+++ pages/api/categories.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { getAllCategories } from '../../lib/utils';
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
+export default (_req: NextApiRequest, res: NextApiResponse) => {
   getAllCategories().then((categories) => {
     res.status(200).json(categories);
   });
